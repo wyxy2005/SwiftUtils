ae47c7e1df2599412784387114ca9471a1dc7300
Alexandre Truppel <alex.truppel@gmail.com>
2015-02-09 17:32:26 +0000

    Still only experiments

--- SwiftUtils/DatePickerTableViewCell.swift
+++ SwiftUtils/DatePickerTableViewCell.swift
@@ -5,7 +5,7 @@
 import UIKit
 import Cartography
 
-public class DynamicTypeLabel: UILabel {
+/*public class DynamicTypeLabel: UILabel {
     required public init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         prepare()
@@ -36,6 +36,73 @@ public class DynamicTypeLabel: UILabel {
         didSet { font = UIFont.preferredFontForTextStyle(textStyle) }
     }
 }
+*/
+
+
+
+public class DynamicTypeManagerClass {
+    private let elementTable = NSMapTable.weakToStrongObjectsMapTable()
+    private let notification: NSObjectProtocol?
+    private class InfoTuple: NSObject {
+        let keyPath: String
+        let textStyle: String
+        init(_ k: String, _ s: String) {
+            keyPath = k
+            textStyle = s
+        }
+    }
+    
+    public init() {
+        notification = NSNotificationCenter.defaultCenter().addObserverForName(UIContentSizeCategoryDidChangeNotification, object: nil, queue: NSOperationQueue.mainQueue()) { [unowned self] _ in
+            let enumerator = self.elementTable.keyEnumerator()
+            while let element = enumerator.nextObject() as NSObject? {
+                let tuple = self.elementTable.objectForKey(element) as InfoTuple
+                element.setValue(UIFont.preferredFontForTextStyle(tuple.textStyle), forKeyPath: tuple.keyPath)
+            }
+        }
+    }
+    
+    public func watch(label: UILabel, textStyle: String? = nil) {
+        watch(label, fontKeypath: "font", textStyle: textStyle)
+    }
+    public func watch(button: UIButton, textStyle: String? = nil) {
+        watch(button, fontKeypath: "titleLabel.font", textStyle: textStyle)
+    }
+    public func watch(textField: UITextField, textStyle: String? = nil) {
+        watch(textField, fontKeypath: "font", textStyle: textStyle)
+    }
+    public func watch(textView: UITextView, textStyle: String? = nil) {
+        watch(textView, fontKeypath: "font", textStyle: textStyle)
+    }
+    public func watch(object: AnyObject, fontKeypath: String, var textStyle: String? = nil) {
+        if textStyle == nil {
+            if let font = object.valueForKey(fontKeypath) as? UIFont {
+                textStyle = textStyleMatchingFont(font)
+            }
+        }
+        
+        if let style = textStyle {
+            object.setValue(UIFont.preferredFontForTextStyle(style), forKeyPath: fontKeypath)
+            elementTable.setObject(InfoTuple(fontKeypath, style), forKey: object)
+        }
+    }
+    
+    public func textStyleMatchingFont(font: UIFont) -> String? {
+        for style in [UIFontTextStyleBody, UIFontTextStyleCaption1, UIFontTextStyleCaption2, UIFontTextStyleFootnote, UIFontTextStyleHeadline, UIFontTextStyleSubheadline] {
+            if font == UIFont.preferredFontForTextStyle(style) {
+                return style
+            }
+        }
+        
+        return nil
+    }
+}
+
+let DynamicTypeManager = DynamicTypeManagerClass()
+
+
+
+
 
 let DatePickerTableViewCellDidExpandNotification = "DatePickerTableViewCellDidExpandNotification"
 
@@ -49,8 +116,8 @@ public class DatePickerTableViewCell: UITableViewCell {
         return df.stringFromDate(date)
     }
     
-    public let leftLabel = DynamicTypeLabel()
-    public let rightLabel = DynamicTypeLabel()
+    public let leftLabel = UILabel()
+    public let rightLabel = UILabel()
     public let datePicker = UIDatePicker()
     
     public var exclusiveExpansion = true
@@ -94,6 +161,10 @@ public class DatePickerTableViewCell: UITableViewCell {
     }
     
     private func prepare() {
+        
+        DynamicTypeManager.watch(leftLabel, textStyle: UIFontTextStyleHeadline)
+        DynamicTypeManager.watch(rightLabel, textStyle: UIFontTextStyleCaption2)
+        
         // Containers
         for view in [labelsContainer, datePickerContainer] {
             contentView.addSubview(view)
